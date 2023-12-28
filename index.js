bbcf31cc7c3c1d0e927ea132482571788f0f91c6
Priyanshu <priyanshukanji0@gmail.com>
2023-12-28 12:59:59 +0530

    This can add new users

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const PORT = 5000;
 const users = require("./MOCK_DATA.json");
 
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
 // Routes
 app.get("/users", (req, res) => {
   //Server side rendering
   const html = `
   <ul> 
- ${users.map((user) => `<li>${user.first_name}</li>`).join("")}} 
+ ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
   </ul>
   `;
   res.send(html);
@@ -33,9 +36,13 @@ app
     //ToDo: Add delete task
     return res.json({ status: "Pending" });
   });
-app.patch("/api/users", (req, res) => {
-  //Todo:Add delete Patch
-  return res.json({ status: "Pending" });
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ id: users.length + 1, ...body });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "new user added", id: users.length });
+  });
+  console.log("Body", body);
 });
 app.listen(PORT, () => {
   console.log(`Server started at PORT:${PORT}`);
